0965036b6ecbb7fe5c4dcd7e87973f121d8d62c2
Alexandre Andre <alexandre.andre---grandjean@utbm.fr>
2024-10-30 17:29:04 +0100

    edit login and signup

--- login.php
+++ login.php
@@ -25,20 +25,20 @@
     </div>
 </nav>
 
-    <h2>Connecter vous à votre compte :</h2>
+<h2>Connecter vous Ã  votre compte :</h2>
 
-    <form method="post" action="../controller/login.php">
+<form method="post" action="c_login.php">
 
-        <label for="mail"  >Email :</label>
-        <input id="mail" class="form-control" name="Email" type="text" required="required" placeholder="email@domain.com" /><br/>
+    <label for="mail"  >Email :</label>
+    <input id="email" class="form-control" name="email" type="text" required="required" placeholder="email@domain.com" /><br/>
 
-        <label for="passwd">Mot de passe :</label>
-        <input id="passwd"  class="form-control" name="Mot de passe" type="password" required="required" placeholder="Entrez votre mot de passe" /><br/>
+    <label for="passwd">Mot de passe :</label>
+    <input id="passwd"  class="form-control" name="passwd" type="password" required="required" placeholder="Entrez votre mot de passe" /><br/>
 
-        <div class="col-12">
-            <button class="btn btn-primary" type="submit">Se connecter</button>
-        </div>
-    </form>
+    <div class="col-12">
+        <button class="btn btn-primary" type="submit">Se connecter</button>
+    </div>
+</form>
 
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-kenU1KFdBIe4zVF0s0G1M5b4hcpxyD9F7jL+jjXkk+Q2h455rYXK/7HAuoJl+0I4" crossorigin="anonymous"></script>
 </body>
--- signup.php
+++ signup.php
@@ -23,25 +23,35 @@
     </div>
 </nav>
 
-<div class="container">
-
 <h2>Veuillez-vous inscrire : </h2>
-<form method="post" action="../controller/signup.php">
+<form method="post" action="c_signup.php">
+
+    <label for="First_name"  >Prénom :</label>
+    <input id="First_name" class="form-control" name="First_name" type="text" required="required" placeholder="Antoine" /><br/>
+
+    <label for="Name"  >Nom :</label>
+    <input id="name" class="form-control" name="name" type="text" required="required" placeholder="Dupont" /><br/>
+
+    <label for="phone"  >Téléphone  :</label>
+    <input id="phone" class="form-control" name="phone" type="tel" required="required" placeholder="+33 6 12 34 56 78" /><br/>
+
+
+
     <label for="mail"  >Email :</label>
     <input id="mail" class="form-control" name="Email" type="text" required="required" placeholder="email@domain.com" /><br/>
 
-    <label for="passwd">Mot de passe :</label>
-    <input id="passwd"  class="form-control" name="Mot de passe" type="password" required="required" placeholder="Entrez votre mot de passe" /><br/>
 
-    <label for="BD">Date de naissance:</label>
-    <input id="BD"  class="form-control" name="birth_date" type="date" value="bd" /><br/><br/>
+    <label for="passwd">Mot de passe :</label>
+    <input id="passwd"  class="form-control" name="passwd" type="password" required="required" placeholder="Entrez votre mot de passe" /><br/>
+
+<!--   PAS UTILE  <label for="BD">Date de naissance:</label>-->
+<!--    <input id="BD"  class="form-control" name="birth_date" type="date" value="bd" /><br/><br/>-->
+
     <div class="col-12">
-    <button class="btn btn-primary" type="submit">S'inscrire</button>
+        <button class="btn btn-primary" type="submit">S'inscrire</button>
     </div>
 
 </form>
-
-</div>
 
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-kenU1KFdBIe4zVF0s0G1M5b4hcpxyD9F7jL+jjXkk+Q2h455rYXK/7HAuoJl+0I4" crossorigin="anonymous"></script>
 </body>

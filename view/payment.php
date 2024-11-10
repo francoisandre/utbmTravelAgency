72230fef73de70f3f4b1149cf9af2d4c3c77412b
Jad-Kahlaoui <jadkahlaouipro@gmail.com>
2024-11-10 02:44:35 +0100

    payment

--- view/payment.php
+++ view/payment.php
@@ -22,28 +22,31 @@ include_once 'common/menu.php' ?>
 
 
 
-<!-- payment.php -->
-<form action="/controller/c_payment.php" method="POST">
-    <label for="id_booking">Booking ID:</label>
-    <input type="text" class="form-control" id="id_booking" name="id_booking" required="required" placeholder="Enter your reservation number" ><br>
+    <!-- payment.php -->
 
-    <label for="amount">Amount:</label>
-   <input type="number" id="amount" name="amount" required="required"><br>
 
-    <label for="payment_method">Payment Method:</label>
-    <select id="payment_method" class="form-control" name="payment_method" required>
-        <option value="Credit Card">Credit Card</option>
-        <option value="Bank Transfer">Bank Transfer</option>
-        <option value="Cash">Cash</option><br>
-    </select>
-<br>
-    <div class="mt-3">
-        <a href="/controller/c_payment.php" class="btn btn-primary">Submit Payment</a>
-    </div>
+    <form action="/controller/c_payment.php" method="POST">
+        <label for="id_booking">Booking ID:</label>
+        <input type="text" class="form-control" id="id_booking" name="id_booking" required="required" placeholder="Enter your reservation number" ><br>
 
-</form>
+        <label for="amount">Amount:</label>
+        <input type="number" id="amount" name="amount" required="required"><br>
+
+        <label for="payment_method">Payment Method:</label>
+        <select id="payment_method" class="form-control" name="payment_method" required>
+            <option value="credit_card">Credit Card</option>
+            <option value="bank_transfer">Bank Transfer</option>
+            <option value="Cash">Cash</option><br>
+        </select>
+        <br>
+        <div class="mt-3">
+            <!-- Bouton de soumission de formulaire -->
+            <button type="submit" class="btn btn-primary">Submit Payment</button>
+        </div>
+    </form>
+
 </body>
-    <?php
-    include_once __DIR__.'/common/footer.php'
-    ?>
+<?php
+include_once __DIR__.'/common/footer.php'
+?>
 </html>
